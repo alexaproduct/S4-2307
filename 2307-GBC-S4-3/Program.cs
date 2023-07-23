6bb2e7b2eae263de752f14b604392e62f00a4473
Alexandra Zotova <alexa@Alexandras-MacBook-Air-2.local>
2023-07-23 23:52:58 +0600

    New commit6

--- 2307-GBC-S4-3/Program.cs
+++ 2307-GBC-S4-3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] arr = new int[8];
+
+// Четные
+int m = 0;
+
+// Нечетные
+int n = 0;
+
+for (int i = 0; i <= 7; i++)
+{
+    arr[i] = new Random().Next(99, 1000);
+    Console.WriteLine(arr[i]);
+    if (arr[i]%2 == 0) m = m+1;
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных: {0}", m);
