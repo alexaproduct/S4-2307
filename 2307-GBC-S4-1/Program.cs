0db6984245ee639f687223d861f56be471b5e6ea
Alexandra Zotova <alexa@Alexandras-MacBook-Air-2.local>
2023-07-23 23:20:56 +0600

    next commit

--- 2307-GBC-S4-1/Program.cs
+++ 2307-GBC-S4-1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+int[] arr = new int[8];
+
+int max = 0;
+int min = 0;
+int delta = 0;
+
+for (int i = 0; i <= 7; i++)
+{
+    arr[i] = new Random().Next(-10, 10);
+    Console.WriteLine(arr[i]);
+}
+
+for (int i = 0; i <= 7; i++)
+{
+    if (arr[i] > max) max = arr[i];
+}
+
+Console.WriteLine();
+Console.WriteLine("Max: {0}", max);
+
+for (int i = 0; i <= 7; i++)
+{
+    if (arr[i] < min) min = arr[i];
+}
+
+Console.WriteLine();
+Console.WriteLine("Min: {0}", min);
+
+delta = max - min;
+Console.WriteLine();
+Console.WriteLine("Разница между Max и Min: {0}", delta);
