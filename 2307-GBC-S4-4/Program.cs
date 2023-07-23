4fb9d65cc5c54fc8c9c80d74355098bdb0202284
Alexandra Zotova <alexa@Alexandras-MacBook-Air-2.local>
2023-07-23 23:59:40 +0600

    New commit10

--- 2307-GBC-S4-4/Program.cs
+++ 2307-GBC-S4-4/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Sasha and Dima!");
+
+// Задача 6 - Показать кубы чисел,
+// заканчивающихся на четную цифру
+
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    if (i%2 == 0)
+    {
+         Console.WriteLine($"{i} x {i} x {i} = {i*i*i}");
+    }
+}
+Console.WriteLine();
